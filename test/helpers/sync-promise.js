146eb7f40ab4ead4b3fc599ec3fd195be53fdd00
zach pratt <zach@attackofzach.com>
2015-03-13 23:43:14 -0500

    Create an instance of a google map once the google maps library has been loaded. Bind a promise to the map becoming idle.

--- test/helpers/sync-promise.js
+++ test/helpers/sync-promise.js
@@ -1,16 +1,26 @@
 var sandbox;
 
 function Promise(resolver) {
+    var thenCallbacks = [],
+        resolved;
+
     this.reject = sandbox.spy();
-    this.resolve = sandbox.spy();
+    this.resolve = sandbox.spy(function () {
+        resolved = true;
+    });
+
+    this.then = function (callback) {
+        thenCallbacks.push(callback);
+
+        if (resolved) {
+            callback();
+        }
+    };
+
     resolver(this.resolve, this.reject);
 }
 
-Promise.prototype.then = function () {
-
-};
-
-export default {
+module.exports = {
     Promise: Promise,
     setSandbox: function (suppliedSandbox) {
         sandbox = suppliedSandbox;
--- test/map-loader.spec.js
+++ test/map-loader.spec.js
@@ -1,20 +1,26 @@
 'use strict';
 
-import MapLoader from '../lib/map-loader';
-import SyncPromise from './helpers/sync-promise.js';
+var fakeGoogle = require('./helpers/fake-google-maps.js'),
+    MapLoader = require('../lib/map-loader'),
+    SyncPromise = require('./helpers/sync-promise.js'),
+    expect = require('chai').expect,
+    sinon = require('sinon'),
 
-import {expect} from 'chai';
-import sinon from 'sinon';
-
-var EXPECTED_GLOBAL_CALLBACK = 'mapLoaded',
+    EXPECTED_GLOBAL_CALLBACK = 'mapLoaded',
     EXPECTED_URL_TEMPLATE = '//maps.googleapis.com/maps/api/js?v={VERSION}&key={KEY}&callback={CALLBACK}',
 
     sandbox,
 
+    fakeMapInstance,
+
     expectedVersion,
     expectedKey,
     expectedUrl,
-    loadOptions;
+    loadOptions,
+    fakeMapOptions,
+    mapContainer,
+
+    mapIdleCallback;
 
 function triggerDocumentReady() {
     var event = document.createEvent('HTMLEvents');
@@ -23,33 +29,59 @@ function triggerDocumentReady() {
     document.dispatchEvent(event);
 }
 
-/*global sinon expect SyncPromise MapLoader */
+function givenGoogleMapsLibHasLoaded() {
+    MapLoader.load(loadOptions);
+
+    global[EXPECTED_GLOBAL_CALLBACK]();
+}
+
+function defineStubs() {
+    sandbox.stub(global, 'Promise', function (resolver) {
+        return new SyncPromise.Promise(resolver);
+    });
+
+    sandbox.stub(fakeGoogle.maps, 'Map')
+        .returns(fakeMapInstance);
+
+    sandbox.stub(fakeGoogle.maps.event, 'addListenerOnce', function (mapInstance, eventName, callback) {
+        if (mapInstance === fakeMapInstance && eventName === 'idle') {
+            mapIdleCallback = callback;
+        }
+    });
+}
+
+function defineFakeData() {
+    fakeMapInstance = {};
+    fakeMapOptions = {};
+    mapContainer = {};
+
+    expectedVersion = '3.20';
+    expectedKey = 'somekey';
+    expectedUrl = EXPECTED_URL_TEMPLATE
+        .replace('{VERSION}', expectedVersion)
+        .replace('{KEY}', expectedKey)
+        .replace('{CALLBACK}', EXPECTED_GLOBAL_CALLBACK);
+
+    loadOptions = {
+        version: expectedVersion,
+        key: expectedKey
+    };
+}
+
 describe('Google Maps Loader Test Suite', function () {
     beforeEach(function () {
         sandbox = sinon.sandbox.create();
-
-        sandbox.stub(global, 'Promise', function (resolver) {
-            return new SyncPromise.Promise(resolver);
-        });
-
         SyncPromise.setSandbox(sandbox);
 
-        expectedVersion = '3.20';
-        expectedKey = 'somekey';
-        expectedUrl = EXPECTED_URL_TEMPLATE
-            .replace('{VERSION}', expectedVersion)
-            .replace('{KEY}', expectedKey)
-            .replace('{CALLBACK}', EXPECTED_GLOBAL_CALLBACK);
-        loadOptions = {
-            version: expectedVersion,
-            key: expectedKey
-        };
+        defineFakeData();
+        defineStubs();
     });
 
     afterEach(function () {
         sandbox.restore();
 
         SyncPromise.unsetSandbox();
+        mapIdleCallback = null;
     });
 
     it('should add the google maps script to the dom', function () {
@@ -62,13 +94,36 @@ describe('Google Maps Loader Test Suite', function () {
     });
 
     it('should resolve the promise once google maps is loaded and the global callback is invoked', function () {
-        var loadPromise = MapLoader.load(loadOptions);
+        var mapLoadPromise = MapLoader.load(loadOptions);
 
-        expect(loadPromise).to.be.an.instanceOf(SyncPromise.Promise);
-
-        sinon.assert.notCalled(loadPromise.resolve);
+        sinon.assert.notCalled(mapLoadPromise.resolve);
         global[EXPECTED_GLOBAL_CALLBACK]();
 
-        sinon.assert.calledOnce(loadPromise.resolve);
+        sinon.assert.calledOnce(mapLoadPromise.resolve);
+    });
+
+    it('should create a map instance with the supplied options', function () {
+        sinon.assert.notCalled(fakeGoogle.maps.Map);
+        givenGoogleMapsLibHasLoaded();
+
+        MapLoader.create(mapContainer, fakeMapOptions);
+
+        sinon.assert.calledOnce(fakeGoogle.maps.Map);
+        sinon.assert.calledWith(fakeGoogle.maps.Map, mapContainer, fakeMapOptions);
+        sinon.assert.calledWithNew(fakeGoogle.maps.Map);
+    });
+
+    it('should resolve with a google maps instance once the map is idle', function () {
+        var mapIdlePromise;
+
+        givenGoogleMapsLibHasLoaded();
+        mapIdlePromise = MapLoader.create(fakeMapOptions);
+
+        sinon.assert.notCalled(mapIdlePromise.resolve);
+
+        mapIdleCallback();
+
+        sinon.assert.calledOnce(mapIdlePromise.resolve);
+        sinon.assert.calledWith(mapIdlePromise.resolve, fakeMapInstance);
     });
 });
