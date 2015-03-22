cd2b961410b4bf1a622f418696216e19895788c6
zach pratt <zach@attackofzach.com>
2015-03-22 18:22:01 -0500

    Expose BaseOverlayFactory on the public API.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var MapLoader = require('./lib/map-loader'),
+    BaseOverlayFactory = require('./lib/base-overlay-factory');
+
+module.exports = {
+    MapLoader: MapLoader,
+    BaseOverlayFactory: BaseOverlayFactory
+};
